48d013166f33cc8917b0ba9c38f7951fe5908d48
Documenter.jl <documenter@juliadocs.github.io>
2025-05-07 01:53:30 +0000

    build based on 3b59640

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#OkValueReplacer","page":"Home","title":"OkValueReplacer","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OkValueReplacer.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
